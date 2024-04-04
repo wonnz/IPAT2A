31d8811bda2ea2feb3d52c7c521a247b2d98e765
Vonn Jules Cabalza <119022485+wonnz@users.noreply.github.com>
2024-04-04 13:08:09 +0800

    Add files via upload

---
+++ IPAT2A/IPAT2A/IPAT2A/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for IPAT2A project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IPAT2A.settings')
+
+application = get_asgi_application()
